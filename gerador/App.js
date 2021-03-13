670713ed6622edf31901447c952196cf3838adba
Programador <brunoferreira.tecnico@gmail.com>
2021-03-13 19:47:08 -0300

    first commit

---
+++ gerador/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import {View, Text} from 'react-native';
+
+
+export default function App(){
+    return(
+        <View style={{ marginTop: 250 }}>
+            <Text style={{ fontSize: 25 }}>Aprendendo React Native</Text>
+            <Text style={{ fontSize: 25 }}>Aprendendo React Native</Text>
+        </View>
+    )
+}
